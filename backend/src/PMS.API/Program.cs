b186bc90d9cf527c88b7790e067eb48389b5fcfb
Gia Phuc <kazyy@MacBook-Pro-cua-Gia.local>
2026-07-23 15:38:44 +0700

    refactor(api): register infrastructure services via AddInfrastructure extension

--- backend/src/PMS.API/Program.cs
+++ backend/src/PMS.API/Program.cs
@@ -1,17 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using PMS.Infrastructure;
 using PMS.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<PmsDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -20,6 +17,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<PmsDbContext>();
+    await context.Database.MigrateAsync();
+    await DbSeeder.SeedAsync(context);
 }
 
 app.UseHttpsRedirection();
